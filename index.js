e62c6877bb421cc5bc6a7f27f4ccf1c4841ebd81
Justine Bricknell <justinegayton@gmail.com>
2018-05-30 17:05:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var index = require("../index-test.js")
 
 function happyHolidays () {
   return 'Happy holidays!';
@@ -15,28 +14,3 @@ function happyHolidayTo (holiday, name){
 function holidayCountdown (days, holiday){
   return 'It\'s is ${days} days until ${holiday}!';
 }
-describe('functions', () => {
-  describe('happyHolidays', () => {
-    it('returns "Happy holidays!"', () => {
-      expect(happyHolidays()).toEqual('Happy holidays!')
-    })
-  })
-
-  describe('happyHolidaysTo(name)', () => {
-    it('returns "Happy holidays, ${name}!"', () => {
-      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
-    })
-  })
-
-  describe('happyHolidayTo(holiday, name)', () => {
-    it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
-    })
-  })
-
-  describe('holidayCountdown(holiday, days)', () => {
-    it('returns "It\'s ${days} days until ${holiday}!"', () => {
-      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-    })
-  })
-})
