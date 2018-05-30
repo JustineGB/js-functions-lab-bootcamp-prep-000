0210573a7076a7d6e4e2fbb1511a5bc06c334c9e
Justine Bricknell <justinegayton@gmail.com>
2018-05-30 17:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ function happyHolidays () {
   return 'Happy holidays!';
 }
 
+var name = "you"
 function happyHolidaysTo (name) {
   return 'Happy holidays, ${name}!';
 }
 
+var holiday = "Indenpendence Day"
 function happyHolidayTo (holiday, name){
   return 'Happy ${holiday}, ${name}!';
 }
 
+var days = 20
+var holiday = "Mother\'s Day"
 function holidayCountdown (days, holiday){
   return 'It\'s is ${days} days until ${holiday}!';
 }
